3c16bdd3e51891c87beb990159e4efb0888c8be0
Ryan Lyman <ryanlyman@gmail.com>
2026-05-22 09:27:19 -0600

    spacing

--- homograph_path.py
+++ homograph_path.py
@@ -215,7 +215,7 @@ def run_non_homograph_tests() -> None:
         passed = (result is False)
         status = "PASS" if passed else "FAIL"
 
-        print(f"\nPath:      {path}")
+        print(f"\nPath:        {path}")
         print(f"Explanation: {explanation}")
         print(f"Canonical:   {canon}")
         print(f"Expected:    False")
@@ -238,7 +238,7 @@ def run_homograph_tests() -> None:
         passed = (result is True)
         status = "PASS" if passed else "FAIL"
 
-        print(f"\nPath:      {path}")
+        print(f"\nPath:        {path}")
         print(f"Explanation: {explanation}")
         print(f"Canonical:   {canon}")
         print(f"Expected:    True")
